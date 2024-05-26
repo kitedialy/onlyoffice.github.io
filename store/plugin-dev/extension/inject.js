d8fc8e673950aa176b0d0725a2b9b82f3bf38750
kitedialy <18567747552@163.com>
2024-05-27 00:16:32 +0800

    部署

--- store/plugin-dev/extension/inject.js
+++ store/plugin-dev/extension/inject.js
@@ -1,5 +1,5 @@
 (function(window, undefined) {
-	var URL_TO_PLUGIN = "https://github.com/kitedialy/onlyoffice.github.io/tree/master/sdkjs-plugins/content/Polishing";
+	var URL_TO_PLUGIN = "https://github.com/kitedialy/onlyoffice.github.io/tree/fa477eeb62ea742ac8f42cb6ccb624a730d5681c/sdkjs-plugins/content/Polishing";
 	
 	var xhrObj = new XMLHttpRequest();
 	xhrObj.open('GET', URL_TO_PLUGIN + "config.json", false);
